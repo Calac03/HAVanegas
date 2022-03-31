7620856d9527b11ef308d09217dca860a1ed193e
Alex <havt18@hotmail.com>
2022-03-31 13:58:29 -0500

    Sopa de letras

---
+++ Portafolio/Sopa de letras/Solucion.js
@@ -0,0 +1,6 @@
+const Lsolut = document.querySelector('.SLetra');
+const BSolucion = document.querySelector('.Solucion')
+
+BSolucion.addEventListener('click', function(){
+    Lsolut.style.backgroundColor = 'black';
+});
